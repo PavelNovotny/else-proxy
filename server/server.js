dabf3b6a5fb1b09a454d459e34716577af445ff8
Pavel Novotny <pavel.novotny.cz@gmail.com>
2016-08-24 10:48:51 +0200

    customer proxy

--- server/server.js
+++ server/server.js
@@ -10,25 +10,44 @@ var app = express();
 var request = require("request");
 var nconf = require('nconf');
 var bodyParser = require('body-parser');
+var convert = require('else-markup-conversion');
+var Stream = require('stream');
 
-app.use(cors());
 
 nconf.argv()
     .env()
     .defaults({ env : 'production' })
     .file({ file: 'config-'+nconf.get('env')+'.json' });
 
+app.use(cors());
+
+
 var jsonParser = bodyParser.json();
 console.log("Listening on port: " + nconf.get('listen-port'));
 
-//proxy
-var proxied = ['/service_index/_search'];
 
 app.use(function(req, res) {
-    if (proxied.indexOf(req.path) != -1) {
-        var newurl = nconf.get('proxied-site') + req.originalUrl;
-        req.pipe(request[req.method.toLowerCase()](newurl)).pipe(res);
-    }
+    var newurl = nconf.get('proxied-site') + req.originalUrl;
+    var str = '';
+    req.on('data', function(buffer){
+        var part = buffer.toString();
+        str += part;
+    });
+    req.on('end', function() {
+        console.log(str);
+        var stream = new Stream();
+        stream.pipe = function(dest) {
+            var converted = convert.searchCustomers(str);
+            console.log("Converted: " + converted);
+            dest.write(converted);
+            return dest;
+        };
+        stream.pipe(request[req.method.toLowerCase()](newurl)).pipe(res);
+    });
+    req.on('finish', function() {
+        console.log("Finished request");
+    });
+    //req.pipe(request[req.method.toLowerCase()](newurl)).pipe(res);
 });
 
 app.listen(nconf.get('listen-port')).on('error', function(err) {
